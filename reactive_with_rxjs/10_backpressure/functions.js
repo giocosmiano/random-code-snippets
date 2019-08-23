3cffcaacb835293e17de43a2d72c9afcc6383f31
u0164744 <giocosmiano@gmail.com>
2019-08-23 19:56:14 -0400

    reactive programming with rxjs

--- reactive_with_rxjs/10_backpressure/functions.js
+++ reactive_with_rxjs/10_backpressure/functions.js
@@ -1,41 +1,45 @@
-var getKeyboardObservable = function(source) {
-  var getWord = function(event) {
+let getKeyboardObservable = function(source) {
+  let getWord = function(event) {
     return event.target.value;
   };
-  
-  return Rx.Observable.fromEvent(source, 'keyup')
-           .map(getWord);
-}
 
-var subscribe = function() {
-  var input = document.getElementById('input');
-  var display = document.getElementById('display');
+  // see Piping --> https://rxjs-dev.firebaseapp.com/guide/operators
+  return rxjs.fromEvent(source, 'keyup')
+      .pipe(rxjs.operators.map(getWord));
+};
+
+let onSubscribe = function() {
+  let input = document.getElementById('input');
+  let display = document.getElementById('display');
   
   input.placeholder = "type something here";
 
-  var convertCase = function(phrase) {
-    var MAX = 50000;
-    for(var i = 0; i < MAX; i++) {
-      for(var j = 0; j < MAX; j++) {
-        var temp = i + j;
+  let convertCase = function(phrase) {
+    let MAX = 50000;
+    for(let i = 0; i < MAX; i++) {
+      for(let j = 0; j < MAX; j++) {
+        let temp = i + j;
       }
     }
     return phrase.toUpperCase(); 
   };
   
-  var displayPhrase = function(phrase) { display.innerHTML = phrase; };
+  let displayPhrase = function(phrase) { display.innerHTML = phrase; };
   
-  var observable = getKeyboardObservable(input);
+  let observable = getKeyboardObservable(input);
 
+  // see Piping --> https://rxjs-dev.firebaseapp.com/guide/operators
   if(document.getElementById('debounce').checked)
-    observable = observable.debounceTime(500);
+    observable =
+        observable.pipe(rxjs.operators.debounceTime(500));
 
   if(document.getElementById('throttle').checked)
-    observable = observable.throttleTime(100);
-    
-    observable.map(convertCase)
+    observable =
+        observable.pipe(rxjs.operators.throttleTime(100));
+
+    observable.pipe(rxjs.operators.map(convertCase))
               .subscribe(displayPhrase);
               
     document.getElementById('debounce').disabled = true;
-    document.getElementById('buffer').disabled = true;
-}
+    document.getElementById('throttle').disabled = true;
+};
