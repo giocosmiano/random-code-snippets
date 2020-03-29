ec06e2bb91d0d095c922589548d18a76f99430eb
gio <giocosmiano@gmail.com>
2020-03-29 09:07:25 -0400

    Exploring Reactive MongoDB with Flux/Mono

--- exploring_reactive_apis/src/main/java/com/giocosmiano/exploration/controller/BookController.java
+++ exploring_reactive_apis/src/main/java/com/giocosmiano/exploration/controller/BookController.java
@@ -5,14 +5,18 @@ import com.giocosmiano.exploration.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
-@RestController
+@Controller
 @RequestMapping(value = "/books")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BookController {
 
     private BookService bookService;
@@ -23,32 +27,52 @@ public class BookController {
 
     private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
+    @ResponseBody
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<Book> getById(@PathVariable("id") String id) {
-        return bookService.getById(Long.valueOf(id));
+    public Mono<ResponseEntity<Book>> getById(@PathVariable("id") String id) {
+        return bookService
+                .getById(Long.valueOf(id))
+                .map(ResponseEntity::ok)
+                ;
     }
 
+    @ResponseBody
     @GetMapping(value = "/isbn/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<Book> getByIsbn(@PathVariable("id") String id) {
-        return bookService.getByIsbn(id);
+    public Mono<ResponseEntity<Book>> getByIsbn(@PathVariable("id") String id) {
+        return bookService
+                .getByIsbn(id)
+                .map(ResponseEntity::ok)
+                ;
     }
 
+    @ResponseBody
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Book> getAllBooks() {
-        return bookService.getAllBooks();
+        return bookService
+                .getAllBooks()
+                ;
     }
 
+    @ResponseBody
     @GetMapping(value = "/streaming", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    public Flux<Book> streamingAllBooks() {
+    public Flux<ResponseEntity<Book>> streamingAllBooks() {
         return bookService
                 .streamingAllBooks()
-                .delayElements(Duration.ofMillis(10)) // delay each element to throttle down streaming of data
+                .delayElements(Duration.ofMillis(5)) // delay each element to throttle down streaming of data to client
+                .map(ResponseEntity::ok)
         ;
     }
 
-    @PostMapping
+    @ResponseBody
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Void> create(@RequestBody Flux<Book> books) {
         return bookService.create(books)
                 .then();
+    }
+
+    @GetMapping(value = "/view")
+    public Mono<String> index(Model model) {
+        model.addAttribute("books", bookService.streamingAllBooks());
+        return Mono.just("redirect:/books_reactor_flux");
     }
 }
