7c36d73ab28ae5d33759a0ca101a309c9c9631c8
Gio Cosmiano <giocosmiano@gmail.com>
2021-06-19 19:30:07 -0400

    Java sorting algorithms

--- code_snippets/src/main/java/sorting/CollectionUtils.java
+++ code_snippets/src/main/java/sorting/CollectionUtils.java
@@ -50,31 +50,36 @@ public class CollectionUtils {
                 .orElse(null);
     }
 
+/**
+ * Running some experiments
     public static <T> Stream<T> streamOfObjects(final Stream<T> stream) {
-        return ofNullable(stream)
-                .map(e -> e.filter(Objects::nonNull))
-                .orElse(Stream.empty());
+        Supplier<Stream<T>> getStream =
+                () -> ofNullable(stream)
+                        .map(e -> e.filter(Objects::nonNull))
+                        .orElse(Stream.empty());
+        return getStream.get();
     }
 
     public static <T> Optional<T> getHead(final Stream<T> stream) {
-        return ofNullable(stream)
-                .map(e -> e.filter(Objects::nonNull))
-                .flatMap(Stream::findFirst);
+        Supplier<Stream<T>> getStream = () -> streamOfObjects(stream);
+        return getStream
+                .get()
+                .findFirst();
     }
 
     public static <T> Stream<T> getTail(final Stream<T> stream) {
-        return ofNullable(stream)
-                .map(e -> e.filter(Objects::nonNull))
-                .map(e -> e.skip(1))
-                .orElse(Stream.empty());
+        Supplier<Stream<T>> getStream = () -> streamOfObjects(stream);
+        return getStream
+                .get()
+                .skip(1);
     }
 
-    public static <T> Optional<ImmutablePair<Optional<T>, Stream<T>>> getHeadAndTail(final Stream<T> stream) {
-        return ofNullable(stream)
-                .map(e -> e.filter(Objects::nonNull))
-                .flatMap(e ->
-                        ofNullable(getHead(e))
-                                .map(e1 -> ImmutablePair.of(e1, getTail(e)))
-                );
+    public static <T> ImmutablePair<T, Stream<T>> getHeadAndTail(final Stream<T> stream) {
+        Supplier<Stream<T>> getStream = () -> streamOfObjects(stream);
+        return
+                getHead(getStream.get())
+                        .map(e -> ImmutablePair.of(e, getTail(getStream.get())))
+                        .orElse(null);
     }
+*/
 }
